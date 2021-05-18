5560b5a37d32dfa0edcb8eeababca050b2db4bbd
wr0n9man <rafigov2017@yandex.ru>
2021-05-18 19:47:46 +0400

    fixed

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -3,8 +3,8 @@ import './header.css'
 export default function Header(){
 	return(		
 			<>
-			<div className="header">
-				<h1>Проверка принадлежности точки многоугольнику методом трассировки луча (ray casting point-in-polygon test).</h1>
+			<div className="header" >
+				<h1 className="header_h1">Проверка принадлежности точки многоугольнику методом трассировки луча (ray casting point-in-polygon test).</h1>
 			</div>
 			</>
 		)
--- src/component/main/main.js
+++ src/component/main/main.js
@@ -1,137 +1,105 @@
 import "./main.css";
-import { Layer, Stage, Line } from "react-konva";
-import Konva from "konva";
-import { useRef, useState } from "react";
+import { Layer, Stage, Line, Circle } from "react-konva";
+
+import {  useEffect, useState } from "react";
 
 	export default function Main() {
 	const [tool, setTool] = useState("pen");
+	const [result, setResult] = useState("")
 	const [gradus, setGradus] = useState(0);
 	const [tangens, setTangens] = useState(0);
-	const [poly, setPoly] = useState([])
+	const [select, setSelect] = useState('polygon')
+	const [pointDot, setPointDot] = useState({
+		Y:-10,
+		X: -10
+	});
+	const [active,setActive]=useState(true);
 	const [lines, setLines] = useState([]);
 	const [dopLine, setDopLine]= useState([]);
-	const isDrawing = useRef(false);
+	const [isDrawingPolygon, setIsDrawingPolygon] = useState(true);
+  const [tutorialStep, setTutorialStep] = useState(1);
+
+	const tutorial ={
+		1:"Нарисуйте прямоугольник",
+		2:"Поставьте точку и нажмите определить",
+		3:"Для того что бы определить проводим лучи от точки, если количество пересечений четное - точка снаружи, если количество пересечений нечетное - точка внутри многоугольника"
+	}
+
+	useEffect(() => {
+		const onKeypress = e => {
+			if(e.key=='c'){
+				setLines([]);
+				setDopLine([]);
+				setIsDrawingPolygon(true);
+			}
+		};
+	
+		document.addEventListener('keypress', onKeypress);
+	
+		return () => {
+			document.removeEventListener('keypress', onKeypress);
+		};
+	}, []);
+
+
 
 	const handleMouseDown = (e) => {
-		isDrawing.current = true;
 		const pos = e.target.getStage().getPointerPosition();
-		
-
-		setLines([...lines, { tool, points: [pos.x, pos.y] }]);	
-		const point = e.target.getStage().getPointerPosition();
-		setDopLine([{ tool, points: [pos.x, pos.y] }]);
-		let lastLine = lines[lines.length - 1];
-
-		// add point
-		if (lastLine!==undefined){
-			lastLine.points = lastLine.points.concat([point.x, point.y]);
-	
-			// replace last
-			lines.splice(lines.length - 1, 1, lastLine);
-		
-		}
-		let polygons=[];
-		console.log(' ');
-		lines.slice(0,lines.length-1).map((line,i)=>{
-
-		let tang = ((dopLine[0].points[3]-((line.points[1]+line.points[3])/2))/(((line.points[0]+line.points[2])/2)-dopLine[0].points[2]))
-		let arctg = Math.atan(tang); // Арктангенс
-		let angle = arctg * 180 / Math.PI; // угол в градусах
-		if (((line.points[1]+line.points[3])/2)>dopLine[0].points[3]){
-			if (tang < 0) {
-				angle = 360 + angle;
-			}else if (tang > 0) {
-				angle = 180 + angle;
-			}			 
-		}else{		
-			if (tang < 0) {
-				angle = 180 + angle;
-			} 
-		}	
-	
-
-
-			console.log(line);
-			console.log((Math.abs(line.points[0]-line.points[2]))<(Math.abs(line.points[1]-line.points[3])));
-			console.log(dopLine[0].points[3],' ',((line.points[0]+line.points[2])/2));
-		
-				let x =0
-				if (((angle<45)&&(angle>0))||((angle<0)&&(angle>315))){
-					x =700;
-					polygons.push({
-						points:[
-						line.points[0],line.points[1],				
-						line.points[2],line.points[3],				
-						x,(line.points[3] + ((700-line.points[2])*(Math.tan( Math.atan((dopLine[0].points[3]-line.points[3]) / (dopLine[0].points[2]-line.points[2])))))),
-						x,(line.points[1] + ((700-line.points[0])*(Math.tan( Math.atan((dopLine[0].points[3]-line.points[1]) / (dopLine[0].points[2]-line.points[0])))))),				
-						],	ugol:{
-							1:angle,
-						}					
-					})
-				}if ((angle<225)&&(angle>135)){
-					polygons.push({
-						points:[
-						line.points[0],line.points[1],				
-						line.points[2],line.points[3],				
-						x,(line.points[3] - (line.points[2]*(Math.tan( Math.atan((dopLine[0].points[3]-line.points[3]) / (dopLine[0].points[2]-line.points[2])))))),
-						x,(line.points[1] - (line.points[0]*(Math.tan( Math.atan((dopLine[0].points[3]-line.points[1]) / (dopLine[0].points[2]-line.points[0])))))),				
-						],			ugol:{
-							1:angle,
-						}					
-					})
-				}
+		if (select=="polygon"){
+			if(isDrawingPolygon){
 			
-				let y=0;
-				
-				if((angle<315)&&(angle>225)){
-					y=700;
-					polygons.push({
-						points:[
-						line.points[0],line.points[1],				
-						line.points[2],line.points[3],				
-						(line.points[2] + ((700-line.points[3])*(Math.tan( Math.atan((dopLine[0].points[2]-line.points[2]) / (dopLine[0].points[3]-line.points[3])))))), y,
-						(line.points[0] + ((700-line.points[1])*(Math.tan( Math.atan((dopLine[0].points[2]-line.points[0]) / (dopLine[0].points[3]-line.points[1])))))), y,				
-						],
-						ugol:{
-							1:angle,
-						}				
-					})
+				setLines([...lines, { tool, points: [pos.x, pos.y] }]);	
+				const point = e.target.getStage().getPointerPosition();
+				setDopLine([{ tool, points: [pos.x, pos.y] }]);
+				console.log(lines);
+				let lastLine = lines[lines.length - 1];
 
-				}if((angle<135)&&(angle>45)){					
-					polygons.push({
-						points:[
-						line.points[0],line.points[1],				
-						line.points[2],line.points[3],				
-						(line.points[2] - (line.points[3]*(Math.tan( Math.atan((dopLine[0].points[2]-line.points[2]) / (dopLine[0].points[3]-line.points[3])))))), y,
-						(line.points[0] - (line.points[1]*(Math.tan( Math.atan((dopLine[0].points[2]-line.points[0]) / (dopLine[0].points[3]-line.points[1])))))), y,				
-						],		ugol:{
-							1:angle,
-							
-						}				
-					})
+				// add point
+				if (lastLine!==undefined){
+					if((lastLine.points[0]!==point.x)&&(lastLine.points[1]!==point.y)){
+					lastLine.points = lastLine.points.concat([point.x, point.y]);
+					lines.splice(lines.length - 1, 1, lastLine);
+				}else{ (lines.splice(lines.length - 1, 1));setLines(lines);
+				}
+				
+					// replace last
+			
+				
+					
+				
 				}
 			}
-			
-		)
-		setPoly(polygons)
+		}else{
 
+			setPointDot({
+				Y:pos.y,
+				X: pos.x
+			})
+			setActive(false);
+		}
+		
 	};
 
-	const handleMouseMove = (e) => {	
+	const handleMouseMove = (e) => {
 		const stage = e.target.getStage();
-		const point = stage.getPointerPosition();	
-		if (lines.find((line)=>{
-			if((dopLine[0].points[0]<line.points[0])&&(dopLine[0].points[0]<line.points[3])){
+		const point = stage.getPointerPosition();		
+		if (select=="polygon"){
+			if(isDrawingPolygon){
 			
+				if (lines.find((line)=>{
+					if((dopLine[0].points[0]<line.points[0])&&(dopLine[0].points[0]<line.points[3])){
+					
+					}
+					if((point.x<=line.points[0]));
+				})){}
+				let lastDopLine = dopLine[0];
+			
+				if (lastDopLine !== undefined){	
+					lastDopLine.points =lastDopLine.points.slice(0,2)
+					lastDopLine.points = lastDopLine.points.concat([point.x, point.y]);			
+					setDopLine(dopLine.concat());		
+				}
 			}
-			if((point.x<=line.points[0]));
-		})){}
-		let lastDopLine = dopLine[0];
-	
-		if (lastDopLine !== undefined){	
-			lastDopLine.points =lastDopLine.points.slice(0,2)
-			lastDopLine.points = lastDopLine.points.concat([point.x, point.y]);			
-			setDopLine(dopLine.concat());		
 		}
 		let tang = ((350-point.y)/(point.x-350))
 		let arctg = Math.atan(tang); // Арктангенс
@@ -149,40 +117,104 @@ import { useRef, useState } from "react";
 		}	
 
 		setGradus(angle)
-		setTangens(tang)		
-		
-
-		
+		setTangens(tang)				
 	};
+
+	const handleDblClick=()=>{
+		if (select=="polygon"){
+			if(isDrawingPolygon){
+				console.log(1);
+				setIsDrawingPolygon(false);
+				if(lines[lines.length-1].points.length==2){lines[lines.length-1].points.push(lines[0].points[0], lines[0].points[1])}else{
+				setLines([...lines, { tool, points: [lines[lines.length-1].points[2], lines[lines.length-1].points[3],lines[0].points[0], lines[0].points[1]] }]);}
+			}
+		
+			// add point
+			setSelect('dot')
+			setTutorialStep(2);
+		}
+	}
+
 	const handleMouseLeave = (e) =>{
 		let lastDopLine = dopLine[0];
 		if (lastDopLine !== undefined){			
 			lastDopLine.points =lastDopLine.points.slice(0,2)
-			lastDopLine.points = lastDopLine.points.splice(lastDopLine.points.length-2,2);
-			
+			lastDopLine.points = lastDopLine.points.splice(lastDopLine.points.length-2,2);			
 			setDopLine(dopLine.concat());
-			console.log(lines,'poly=',poly);
+			console.log(lines);
 			console.log(dopLine);
 		}
 		
 	}
+	
 
-	const handleMouseUp = () => {
-		isDrawing.current = false;
+	
+
+	function classify(line, point) {
+    var pr = (line.points[2] - line.points[0]) * (point.Y - line.points[1]) - 
+             (line.points[3] - line.points[1]) * (point.X - line.points[0]);
+						
+    if (pr > 0)
+        return 1;
+    if (pr < 0)
+        return -1;
+    return 0;
+}
+
+	// классифицируем ребро (Касается, пересекает или безразлично)
+	function edgeType(line, point) {
+		switch (classify(line, point)) {
+				case 1:				
+						return ( (line.points[1] < point.Y) && (point.Y <= line.points[3]) ) ? 1 : 2;
+				case -1:
+						return ((line.points[3] < point.Y) && (point.Y <= line.points[1])) ? 1 : 2;
+				case 0:
+						return 0;
+						
+		}
+}
+
+function pointInPolygon(lines, dot) {
+	let parity = 0;
+	lines.map((line)=>{
+		console.log(edgeType(line, dot));
+		switch (edgeType(line, dot)) {
+			case 0:
+					return 2;						
+			case 1:
+					parity = 1 - parity;
+					break;
+			default: break;
+	}
+	})
+	return parity;
+}
+
+
+	function handleClickButton(){	
+		setTutorialStep(3);
+	var checkP = pointInPolygon(lines, pointDot)
+		if(checkP == 0){setResult('Точка лежит вне многоугольника')}
+		else if(checkP == 1) {setResult('Точка лежит в многоугольнике');}else{
+		setResult('Точка лежит на грани многоугольника');}
 		
-	};
+		
+	}
 
 
   return (
-	  <>
-	  <label>градусы={gradus}, Тангенс={tangens}</label>
+	  <div className="main">
+	  {/* <label>градусы={gradus}, Тангенс={tangens}</label> */}
+		
 		<Stage
 			width={700}
 			height={700}
 			onClick={handleMouseDown}
+			onDblClick={handleDblClick}
 			onMousemove={handleMouseMove}
 			onMouseLeave={handleMouseLeave}
-			onMouseup={handleMouseUp}
+			
+	
 			className="main__canvas"
 		>
 			<Layer>
@@ -212,23 +244,24 @@ import { useRef, useState } from "react";
 						}
 					/>
 				))}
-					{poly.map((poly, i) => (
-					<Line
-						key={i}
-						points={poly.points}
-						stroke="#3d3d3d"
-						strokeWidth={1}
-						fill='#00D2FF' 
-						lineCap="round"
-						closed="true"
-						opacity={0.3}				
-						globalCompositeOperation={
-							dopLine.tool === "eraser" ? "destination-out" : "source-over"
-						}
-					/>
-				))}
+				<Circle				
+					x= {pointDot.X}
+					y={pointDot.Y}
+					radius={1}
+					fill='red'
+					stroke= 'red'
+					strokeWidth ={1}
+				/>
 			</Layer>
 		</Stage>
-		</>
+		<div className="main__interface">	
+			<button className="main__button" disabled={active} onClick={handleClickButton}>Определить</button>
+			<label className="main__clear" >Что бы отчистить поле нажмите "C"(clear) </label>
+			<label className="main__tutorial">{tutorial[tutorialStep]}</label>
+
+			<label className="main__result">{result}</label>
+		</div>
+	
+		</div>
 	);
 }
